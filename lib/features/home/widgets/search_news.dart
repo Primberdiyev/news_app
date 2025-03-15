64ae5307d6765b13b5182e44ec163c13b52ceaa1
Dilshodbek <Dilshoddeveloper2005@gmail.com>
2025-03-15 23:24:50 +0500

    debounce is added

--- lib/features/home/widgets/search_news.dart
+++ lib/features/home/widgets/search_news.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/features/home/bloc/home_bloc.dart';
@@ -14,10 +16,21 @@ class SearchNews extends StatefulWidget {
 
 class _SearchNewsState extends State<SearchNews> {
   final TextEditingController controller = TextEditingController();
+  Timer? _debounce;
   @override
   void dispose() {
     controller.dispose();
+    _debounce?.cancel();
     super.dispose();
+  }
+
+  void _onSearchChanged(String value) {
+    if (_debounce?.isActive ?? false) {
+      _debounce?.cancel();
+    }
+    _debounce = Timer(Duration(milliseconds: 500), () {
+      context.read<HomeBloc>().add(FilterNewsEvent(value, news: widget.news));
+    });
   }
 
   @override
@@ -30,9 +43,7 @@ class _SearchNewsState extends State<SearchNews> {
           Expanded(
             child: TextField(
               controller: controller,
-              onChanged: (value) => context
-                  .read<HomeBloc>()
-                  .add(FilterNewsEvent(controller.text, news: widget.news)),
+              onChanged: (value) => _onSearchChanged(value),
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
